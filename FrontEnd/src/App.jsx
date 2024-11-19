f080daea2a409a3274fd7d6d255098816d487e93
Deysehgfi <deyseane2006@gmail.com>
2024-11-19 17:09:21 -0300

    Implementando o react Router

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,15 @@
-import Home from "./Pages/Home.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login.jsx";
 
 
 const App = () => {
     return (
         <>
-        <Home/>
+           <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
         </>
     )
 }
