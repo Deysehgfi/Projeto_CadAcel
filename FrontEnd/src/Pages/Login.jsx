90cb2cb941e7b46a2100c1b0e54c98d05e2b3b39
Deysehgfi <deyseane2006@gmail.com>
2024-11-20 10:42:33 -0300

    HeaderMobile inicializado

--- FrontEnd/src/Pages/Login.jsx
+++ FrontEnd/src/Pages/Login.jsx
@@ -3,12 +3,14 @@ import "../index.css"
 import Section01 from "../Section01/Section01.jsx";
 import Section2 from "../Section02/Section02.jsx";
 import Footer from "../Footer/Footer.jsx";
+import HeaderMobile from "../Header/HeaderMobile.jsx";
 
 
 const Home = () => {
     return (
         <>
             <Header />
+            <HeaderMobile/>
             <Section01 />
             <Section2 />
             <Footer />
