f5211daca961375920512da4747d5aa4af6bcdec
Deysehgfi <deyseane2006@gmail.com>
2024-11-26 20:03:52 -0300

    Ajustes no form do cadastro

--- FrontEnd/src/Pages/Cadastro.jsx
+++ FrontEnd/src/Pages/Cadastro.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import axios from "axios"
-import { useNavigate } from "react-router-dom"
 import Input from "../Components/Input/Input.jsx"
 import Botao from "../Components/Botao/Botao.jsx";
 import { Form, LinkForm, TituloForm, BOxTitulo, Select, SectionCadastro} from "../styled/Cadastro.js";
@@ -13,24 +12,24 @@ import IconAnexo from "../public/Anexo.svg"
 
 
 
-const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitulo, NameLabel, IconImg, FuncaoBotao }) => {
+const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitulo, NameLabel, IconImg, FuncaoBotao, TipoBotao }) => {
     const [nome, setNome] = useState("")
     const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [data, setData] = useState("")
-    const [number, setNumber] = useState("")
+    const [senha, setSenha] = useState("")
+    const [dataNascimento, setDataNascimento] = useState("")
+    const [telefone, setTelefone] = useState("")
     const [deficiencia, setDeficiencia] = useState("")
     const [error, setError] = useState(null)
     const [user, setUser] = useState(null);
     
     const handleCadastro = async (event) => {
-        event.preventDefault()
+        event.preventDefault();
         try {
-            const response = await axios.post(
+            await axios.post(
                 "http://localhost:3333/usuarios/cadastro",
-                JSON.stringify({ nome, email, password, data, number, deficiencia}),
+                JSON.stringify({ nome, email, senha, telefone, deficiencia, dataNascimento}),
                 {
-                    headers: { "Content-Type": "application/json" },
+                    headers: { "Content-Type": "application/json" }
                 },
             );
             setUser(response.data)
@@ -39,35 +38,39 @@ const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitul
             if (!error?.response) {
                 setError("Erro ao acessar o site");
             } else if (error.response.status === 401) {
-                setError(" Usuário ou senha inválidos",);
+                setError("Usuário ou senha inválidos",);
             }
         }
     }
-
+console.log("Usuario ->>>", nome,senha,"<-- nome")
 
 
     return (
         <>
         <SectionCadastro>
-        <Form>
+        <Form onSubmit={handleCadastro}>
             <BOxTitulo>
                 <TituloForm>Cadastro</TituloForm>
             </BOxTitulo>
-            <Input TipoInput="text" NameLabel="nome" IconImg={Person} onChange={(event) => setNome(event.target.value)}/>
-            <Input TipoInput="email" NameLabel="email"  IconImg={IconEmail} onChange={(event) => setEmail(event.target.value)}/>
-            <Input TipoInput="password" NameLabel="senha"  IconImg={IconSenha} onChange={(event) => setPassword(event.target.value)}/>
-            <Input TipoInput="data" NameLabel="data de nascimento"IconImg={Icondata} onChange={(event) => setData(event.target.value)}/>
-            <Input TipoInput="number" NameLabel="telefone" IconImg={Iconphone} onChange={(event) => setNumber(event.target.value)}/>
-            <Select name="" id="" onChange={(event) => setDeficiencia(event.target.value)}>
-                <option value="">deficiência</option>
-                <option value="">Visual</option>
-                <option value="">Fisica</option>
-                <option value="">Auditiva</option>
-                <option value="">Neurodivergente</option>
+            <Input TipoInput="text" NameLabel="nome" value={nome} IconImg={Person} onChange={(event) => setNome(event.target.value)}/>
+
+            <Input TipoInput="email" NameLabel="email" value={email} IconImg={IconEmail} onChange={(event) => setEmail(event.target.value)}/>
+
+            <Input TipoInput="password" NameLabel="senha" value={senha}  IconImg={IconSenha} onChange={(event) => setSenha(event.target.value)}/>
+
+            <Input TipoInput="data" NameLabel="data de nascimento"IconImg={Icondata} value={dataNascimento} onChange={(event) => setDataNascimento(event.target.value)}/>
+
+            <Input TipoInput="number" NameLabel="telefone" value={telefone} IconImg={Iconphone} onChange={(event) => setTelefone(event.target.value)}/>
+
+            <Select value={deficiencia} onChange={(event) => setDeficiencia(event.target.value)}>
+                <option>deficiência</option>
+                <option>Visual</option>
+                <option>Fisica</option>
+                <option>Auditiva</option>
+                <option>Neurodivergente</option>
             </Select>
-            <Input TipoInput="text" NameLabel="anexo do comprovante de deficiência"  IconImg={IconAnexo}/>
             {error}
-            <Botao nomeBotao="Cadastrar" onClick={(event) => handleCadastro(event)} />
+            <Botao nomeBotao="Cadastrar" TipoBotao="submit"/>
         </Form>
 
         </SectionCadastro>
