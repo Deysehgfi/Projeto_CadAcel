16bd8083197183ecf4c476a140ef5a18e510336a
deyseane <deyseane2006@gmail.com>
2024-11-27 08:00:51 -0300

    Ligação back com front da pag de cadastro finalida

--- FrontEnd/src/Components/Input/Input.jsx
+++ FrontEnd/src/Components/Input/Input.jsx
@@ -2,10 +2,10 @@ import {InputStyle,InputFields, Label, ImgIcon} from "../../styled/Input.js"
 import "../../index.css"
 import iconEmail from "../../public/Email.svg"
 
-const Input = ({TipoInput, NomeInput, NameLabel, IconImg, tipoDeUseState, Index, FuncaoInput}) => {
+const Input = ({TipoInput, NomeInput, NameLabel, IconImg, tipoDeUseState, Index, FuncaoInput, valorinput}) => {
     return(
         <InputFields className="input-fields">
-        <InputStyle tabIndex={Index} type={TipoInput} name={NomeInput} placeholder=""  required/>
+        <InputStyle tabIndex={Index} value={valorinput} type={TipoInput} name={NomeInput} placeholder=""  required/>
         <Label> <ImgIcon src={IconImg} alt="" className="imgIConEmail" />{NameLabel}</Label>
         </InputFields>
     )
--- FrontEnd/src/Pages/Cadastro.jsx
+++ FrontEnd/src/Pages/Cadastro.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import axios from "axios"
 import Input from "../Components/Input/Input.jsx"
 import Botao from "../Components/Botao/Botao.jsx";
-import { Form, LinkForm, TituloForm, BOxTitulo, Select, SectionCadastro} from "../styled/Cadastro.js";
+import { Form, LinkForm, TituloForm, BOxTitulo, Select, SectionCadastro } from "../styled/Cadastro.js";
+import { InputStyle, InputFields, Label, ImgIcon } from "../styled/Input.js"
 import IconEmail from "../public/Email-Icon.svg"
 import Person from "../public/Group.svg"
 import IconSenha from "../public/Senha.svg"
 import Icondata from "../public/DataNasc.svg"
 import Iconphone from "../public/Telephone.svg"
-import IconAnexo from "../public/Anexo.svg"
 
 
 
-const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitulo, NameLabel, IconImg, FuncaoBotao, TipoBotao }) => {
+const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitulo, NameLabel, valorinput, IconImg, FuncaoBotao, TipoBotao }) => {
     const [nome, setNome] = useState("")
     const [email, setEmail] = useState("")
     const [senha, setSenha] = useState("")
@@ -21,13 +21,13 @@ const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitul
     const [deficiencia, setDeficiencia] = useState("")
     const [error, setError] = useState(null)
     const [user, setUser] = useState(null);
-    
+
     const handleCadastro = async (event) => {
         event.preventDefault();
         try {
             await axios.post(
                 "http://localhost:3333/usuarios/cadastro",
-                JSON.stringify({ nome, email, senha, telefone, deficiencia, dataNascimento}),
+                JSON.stringify({ nome, email, senha, telefone, deficiencia, dataNascimento }),
                 {
                     headers: { "Content-Type": "application/json" }
                 },
@@ -41,39 +41,64 @@ const Cadastro = ({ TipoInput, NomeInput, PlaceholderInput, nomeBotao, FormTitul
                 setError("Usuário ou senha inválidos",);
             }
         }
+
+        console.log("NOme ->", nome)
+        console.log("senha->", senha)
+        console.log("data->", dataNascimento)
+        console.log("email->", email)
+        console.log("telefone->", telefone)
+        console.log("deficiencia->", deficiencia)
     }
-console.log("Usuario ->>>", nome,senha,"<-- nome")
 
 
     return (
         <>
-        <SectionCadastro>
-        <Form onSubmit={handleCadastro}>
-            <BOxTitulo>
-                <TituloForm>Cadastro</TituloForm>
-            </BOxTitulo>
-            <Input TipoInput="text" NameLabel="nome" value={nome} IconImg={Person} onChange={(event) => setNome(event.target.value)}/>
+            <SectionCadastro>
+                <Form onSubmit={handleCadastro}>
+                    <BOxTitulo>
+                        <TituloForm>Cadastro</TituloForm>
+                    </BOxTitulo>
 
-            <Input TipoInput="email" NameLabel="email" value={email} IconImg={IconEmail} onChange={(event) => setEmail(event.target.value)}/>
+                    <InputFields className="input-fields">
+                        <InputStyle type="text" value={nome} onChange={(event) => setNome(event.target.value)} placeholder="" required />
+                        <Label> <ImgIcon src={Person} alt="" className="imgIConEmail" />nome</Label>
+                    </InputFields>
 
-            <Input TipoInput="password" NameLabel="senha" value={senha}  IconImg={IconSenha} onChange={(event) => setSenha(event.target.value)}/>
 
-            <Input TipoInput="data" NameLabel="data de nascimento"IconImg={Icondata} value={dataNascimento} onChange={(event) => setDataNascimento(event.target.value)}/>
+                    <InputFields className="input-fields">
+                        <InputStyle type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="" required />
+                        <Label> <ImgIcon src={IconEmail} alt="" className="imgIConEmail" />email</Label>
+                    </InputFields>
 
-            <Input TipoInput="number" NameLabel="telefone" value={telefone} IconImg={Iconphone} onChange={(event) => setTelefone(event.target.value)}/>
+                    <InputFields className="input-fields">
+                        <InputStyle type="password" value={senha} onChange={(event) => setSenha(event.target.value)} placeholder="" required />
+                        <Label> <ImgIcon src={IconSenha} alt="" className="imgIConEmail" />senha</Label>
+                    </InputFields>
 
-            <Select value={deficiencia} onChange={(event) => setDeficiencia(event.target.value)}>
-                <option>deficiência</option>
-                <option>Visual</option>
-                <option>Fisica</option>
-                <option>Auditiva</option>
-                <option>Neurodivergente</option>
-            </Select>
-            {error}
-            <Botao nomeBotao="Cadastrar" TipoBotao="submit"/>
-        </Form>
 
-        </SectionCadastro>
+                    <InputFields className="input-fields">
+                        <InputStyle type="date" value={dataNascimento} onChange={(event) => setDataNascimento(event.target.value)} placeholder="" required />
+                        <Label> <ImgIcon src={Icondata} alt="" className="imgIConEmail" />data de nascimento</Label>
+                    </InputFields>
+
+
+                    <InputFields className="input-fields">
+                        <InputStyle type="number" value={telefone} onChange={(event) => setTelefone(event.target.value)} placeholder="" required />
+                        <Label> <ImgIcon src={Iconphone} alt="" className="imgIConEmail"/>telefone</Label>
+                    </InputFields>
+
+                    <Select value={deficiencia} onChange={(event) => setDeficiencia(event.target.value)}>
+                        <option>deficiência</option>
+                        <option>Visual</option>
+                        <option>Fisica</option>
+                        <option>Auditiva</option>
+                        <option>Neurodivergente</option>
+                    </Select>
+                    <p>{error}</p>
+                    <Botao nomeBotao="Cadastrar" TipoBotao="submit" />
+                </Form>
+
+            </SectionCadastro>
         </>
     )
 
